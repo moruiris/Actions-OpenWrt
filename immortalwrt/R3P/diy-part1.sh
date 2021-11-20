142626f3811388e6a0fca31301378e3d3b43ed57
moruiris <53965900+moruiris@users.noreply.github.com>
2021-11-20 22:49:40 +0800

    Update diy-part1.sh

--- immortalwrt/R3P/diy-part1.sh
+++ immortalwrt/R3P/diy-part1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #
+git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng ./package/luci-app-chinadns-ng
+git clone https://github.com/pexcn/openwrt-chinadns-ng ./package/openwrt-chinadns-ng
+git clone https://github.com/kongfl888/luci-app-adguardhome ./package/luci-app-adguardhome
