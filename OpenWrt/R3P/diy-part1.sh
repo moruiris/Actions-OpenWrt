3662638ed649f848dbd02dbf6b35910d02ff4d07
moruiris <53965900+moruiris@users.noreply.github.com>
2021-12-19 22:06:20 +0800

    Update diy-part1.sh

--- OpenWrt/R3P/diy-part1.sh
+++ OpenWrt/R3P/diy-part1.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #
-svn co  https://github.com/moruiris/openwrt-packages/trunk/luci-app-adguardhome ./package/moruiris/luci-app-adguardhome
-svn co  https://github.com/moruiris/openwrt-packages/trunk/luci-app-chinadns-ng ./package/moruiris/luci-app-chinadns-ng
-svn co  https://github.com/moruiris/openwrt-packages/trunk/openwrt-passwall/chinadns-ng ./package/moruiris/chinadns-ng
-cd ./package/moruiris
-git clone https://github.com/jerrykuku/luci-theme-argon.git
+
