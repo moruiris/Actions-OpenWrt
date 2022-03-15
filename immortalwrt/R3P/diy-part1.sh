023a8820f23156d1dc549531e200fd815cafbcf4
moruiris <53965900+moruiris@users.noreply.github.com>
2022-03-15 15:14:51 +0800

    Update diy-part1.sh

--- immortalwrt/R3P/diy-part1.sh
+++ immortalwrt/R3P/diy-part1.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 #
-svn co  https://github.com/moruiris/openwrt-packages/trunk/luci-app-chinadns-ng ./package/moruiris/luci-app-chinadns-ng
-svn co  https://github.com/moruiris/openwrt-packages/trunk/luci-app-adguardhome ./package/moruiris/luci-app-adguardhome
-
