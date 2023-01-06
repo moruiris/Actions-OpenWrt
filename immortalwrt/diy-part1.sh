1d653bc8c40261a98643d4a6080388ec4567518e
moruiris <53965900+moruiris@users.noreply.github.com>
2023-01-06 15:44:38 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # Add a feed source
-echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;openwrt' >>feeds.conf.default
+echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;immortalwrt' >>feeds.conf.default
