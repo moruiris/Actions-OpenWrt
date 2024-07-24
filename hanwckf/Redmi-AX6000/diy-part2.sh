70960ee176701b3f172ce5722e6a5e1ca145a25f
Rapheal <24764156+RaphealK@users.noreply.github.com>
2024-07-24 17:03:34 +0800

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -11,12 +11,27 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-##-----------------Add OpenClash dev core------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-##---------------------------------------------------------
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+#删除默认登陆密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+# Modify hostname
+sed -i 's/ImmortalWrt/RaphaelWrt/g' package/base-files/files/bin/config_generate
+
+#修改wifi名称（mtwifi-cfg）
+sed -i 's/ImmortalWrt-2.4G/LeslieForever/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ImmortalWrt-5G/LeslieForever_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+# 修改默认wifi密码key为1234567890
+sed -i 's/encryption=none/encryption=dg810221/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+#修改闪存为256M版本(这是针对原厂128闪存来的，但又要编译256M固件来的）
+#sed -i 's/<0x580000 0x7200000>/<0x580000 0xee00000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
+#sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
+
+#删除冲突的软件包
+#rm -rf ./package/istore
+#rm -rf ./feeds/kenzo/luci-app-quickstart
+#rm -rf ./feeds/kenzo/luci-app-store
+#rm -rf ./feeds/kenzo/luci-lib-taskd
