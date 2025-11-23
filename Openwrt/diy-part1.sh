eda09726fca15017307beb896ed08ade5118fec4
morui <53965900+moruiris@users.noreply.github.com>
2025-11-23 17:18:33 +0800

    Update diy-part1.sh

--- Openwrt/diy-part1.sh
+++ Openwrt/diy-part1.sh
@@ -2,8 +2,8 @@
 #
 # Add a feed source
 #1·在feeds.conf.default文件首行添加源码
-#sed -i '1i src-git moruiris https://github.com/moruiris/openwrt-packages;openwrt' feeds.conf.default
+sed -i '1i src-git moruiris https://github.com/moruiris/openwrt-packages;openwrt' feeds.conf.default
 #2·在feeds.conf.default文件末尾添加源码
 #echo 'src-git moruiris https://github.com/moruiris/openwrt-packages;openwrt' >>feeds.conf.default
 #3·直接在./package添加源码
-git clone -b openwrt https://github.com/moruiris/openwrt-packages ./package/moruiris
+#git clone -b openwrt https://github.com/moruiris/openwrt-packages ./package/moruiris
